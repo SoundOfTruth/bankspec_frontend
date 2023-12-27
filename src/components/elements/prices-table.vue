5c9f0b9bb9710c64c8f4112d753e089d53d3f9e4
SoundOfTruth <124897561+SoundOfTruth@users.noreply.github.com>
2023-12-28 01:00:51 +0600

    Update prices-table.vue

--- src/components/elements/prices-table.vue
+++ src/components/elements/prices-table.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="st">
     <p class="cg">Название банка</p>
-    <p class="cg">Продажа USD</p>
-    <p class="cg">Покупка USD</p>
+    <p class="cg">
+      Покупка USD
+      <arrows-price @click="pricesSort('USDBuy')" />
+    </p>
+    <p class="cg">
+      Продажа USD
+      <arrows-price @click="pricesSort('USDSell')" />
+    </p>
   </div>
   <div v-for="(el, idx) in props.prices" :key="idx" class="st">
     <p>{{ el.BankName }}</p>
@@ -12,7 +18,21 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import plusSvg from "../../components/svg/plus-svg.vue";
+import ArrowsPrice from "../svg/arrows-price.vue";
+
 const props = defineProps(["prices"]);
+let isFirst = ref(true);
+
+const pricesSort = (val: string) => {
+  if (isFirst.value) {
+    isFirst.value = false;
+    return props.prices.sort((a, b) => a[val] - b[val]);
+  }
+  isFirst.value = true;
+  return props.prices.sort((z, x) => x[val] - z[val]);
+};
 </script>
 
 <style lang="scss" scoped>
